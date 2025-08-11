2e4a4bb9c3a1b0b0f98d7146da369889ff1383c1
AyushGamer55 <141685178+AyushGamer55@users.noreply.github.com>
2025-08-11 14:53:03 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,7 +39,7 @@ function Header({
         <img
           src={logo}
           alt="Spider Logo"
-          className="h-16 w-auto object-contain drop-shadow-[0_0_10px_#f00]"
+          className="h-14 w-auto object-contain drop-shadow-[0_0_10px_#f00]"
         />
         <div>
           <h1 className="text-3xl md:text-4xl font-bold text-cyan-400 tracking-wider drop-shadow-[0_0_10px_#0ff]">
