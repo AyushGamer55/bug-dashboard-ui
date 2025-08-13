389877cb26e0af2acf4ef60e8d7487ed6f2572bf
AyushGamer55 <141685178+AyushGamer55@users.noreply.github.com>
2025-08-13 13:29:24 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header({
   setSearch,
   toggleTheme,
   theme,
-  onShowSummary 
+  onOpenSummary 
 }) {
   return (
     <div className="glass relative flex flex-col md:flex-row justify-between items-center gap-4 mb-6 p-6 border border-cyan-400 shadow-lg">
@@ -93,7 +93,7 @@ function Header({
 
         {/* Generate Summary */}
         <button
-          onClick={onShowSummary}
+          onClick={onOpenSummary}
           className="btn bg-orange-500 text-black hover:bg-orange-400"
         >
           Generate Summary 📊
