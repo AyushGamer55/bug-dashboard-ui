d1bda5c47e2b0ceceb78beb40cc601fde0fb0407
AyushGamer55 <141685178+AyushGamer55@users.noreply.github.com>
2025-08-14 14:12:25 +0530

    Update BugList.jsx

--- src/components/BugList.jsx
+++ src/components/BugList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import BugCard from './BugCard'
 
-function BugList({ bugs, editMode, handleDelete, handleUpdate }) {
+function BugList({ bugs, editMode, handleDelete, handleUpdate, onToggleEdit }) {
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
       {bugs.map((bug, i) => (
@@ -11,7 +11,7 @@ function BugList({ bugs, editMode, handleDelete, handleUpdate }) {
           editMode={editMode}
           onDelete={() => handleDelete(bug._id)}
           onUpdate={(updatedFields) => handleUpdate(bug._id, updatedFields)}
-          onToggleEdit={() => setEditMode((prev) => !prev)}
+          onToggleEdit={onToggleEdit}
         />
       ))}
     </div>
