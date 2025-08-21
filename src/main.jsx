464428134ca73601e830bae50c1e877448dbd54a
AyushGamer55 <141685178+AyushGamer55@users.noreply.github.com>
2025-08-21 13:16:17 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { SpeedInsights } from "@vercel/speed-insights/react"   
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <App />
     <ToastContainer position="top-right" autoClose={2500} />
+    <SpeedInsights /> 
   </>
 )
