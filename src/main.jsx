f2ba01c6f88ce6a3873029b55863486cf2a9819b
AyushGamer55 <141685178+AyushGamer55@users.noreply.github.com>
2025-08-29 10:55:41 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx';
 import './index.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { SpeedInsights } from "@vercel/speed-insights/react";
 import { AuthProvider } from './context/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -13,7 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App />
       <ToastContainer position="top-right" autoClose={2500} />
-      <SpeedInsights />
     </BrowserRouter>
   </AuthProvider>
 );
